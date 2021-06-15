c3378bc5f683e4eb9bfcc9bdcbf5d9d1f249da4c
brunojviana <brunojviana@hotmail.com>
2021-06-14 22:23:45 -0300

    Ajustes na função de autenticação

--- src/main/kotlin/servidor/ServidorApi.kt
+++ src/main/kotlin/servidor/ServidorApi.kt
@@ -59,13 +59,13 @@ fun Route.meuindex() {
                 h1 { +"Banco de Preços de Combustíveis" }
                 p { +"Obtenha informações de preços de combustíveis por localidade, além de previsões com base nas cotações do Dólar e do Barril de Petróleo Brent" }
                 ul {
-                    ol { +"POST - /usuario                      - Cadastra um usuário"}
-                    ol { +"GET  - /usuario/ativo                - Consulta usuário ativo"}
-                    ol { +"POST - /login                        - Efetua login com email e senha"}
-                    ol { +"POST - /commoditie/combustivel/local - Cadastra Local do Combustivel"}
-                    ol { +"POST - /commoditie/combustivel       - Cadastra Preço de Combustível" }
-                    ol { +"GET  - /precos/media                 - Consulta Preço Médio por Estado"}
-                    ol { +"GET  - /precos/estado                - Consulta Menor Preço no Muncípio"}
+                    ol { +"POST - /usuario                      - Cadastrar um usuário"}
+                    ol { +"GET  - /usuario/ativo                - Consultar usuário ativo"}
+                    ol { +"POST - /login                        - Efetuar login com email e senha"}
+                    ol { +"POST - /commoditie/combustivel/local - Cadastrar Local do Combustivel"}
+                    ol { +"POST - /commoditie/combustivel       - Cadastrar Preço de Combustível" }
+                    ol { +"GET  - /precos/media                 - Consultar Preço Médio por Estado"}
+                    ol { +"GET  - /precos/menor                 - Consultar Menor Preço no Muncípio"}
                     ol { +"GET  - /precos/modelo                - Treinar Modelo de Regressão"}
                     ol { +"GET  - /precos/modelo/previsao       - Calcular Previsão do Preço do Combustível"}
                 }
@@ -103,7 +103,7 @@ fun Route.usuario() {
             return@post
         }
         bancoprecos.criaUsuario(novo.nome!!, novo.email!!, novo.senha!!)
-        call.respond(HttpStatusCode.Created)
+        call.respond(HttpStatusCode.Created, "Usuário criado com sucesso.")
     }
 }
 
@@ -112,7 +112,7 @@ fun Route.login() {
         var usuario: Usuario = call.receive<Usuario>()
 
         if (usuario.email == null) {
-            call.respond(HttpStatusCode.BadRequest, "O E-mail é obrigatório para efetuar o Login!")
+            call.respond(HttpStatusCode.BadRequest, "O e-mail é obrigatório para efetuar o Login!")
             return@post
         }
         if (usuario.senha == null) {
@@ -126,87 +126,136 @@ fun Route.login() {
             call.respond(HttpStatusCode.BadRequest, erro)
             return@post
         }
-        call.respond(HttpStatusCode.NoContent)
+        call.respond(HttpStatusCode.OK, "Login efetuado com sucesso.")
     }
 }
 
 fun Route.cadastraLocalCombustivel(): Revenda {
     val novoLoc: Revenda = Revenda()
     post("/commoditie/combustivel/local"){
-        val localCombustivel: Revenda = call.receive<Revenda>()
-        val localCadastrado = bancoprecos.cadastraLocalCombustivel(
-            localCombustivel.regiao,
-            localCombustivel.siglaEstado,
-            localCombustivel.municipio,
-            localCombustivel.nome,
-            localCombustivel.cnpj,
-            localCombustivel.bandeira
-        )
-        novoLoc.regiao = localCadastrado.regiao
-        novoLoc.siglaEstado = localCadastrado.siglaEstado
-        novoLoc.municipio = localCadastrado.municipio
-        novoLoc.nome = localCadastrado.nome
-        novoLoc.cnpj = localCadastrado.cnpj
-        novoLoc.bandeira = localCadastrado.bandeira
-        call.respond(localCadastrado)
+        val usuario = bancoprecos.usuarioAtivo
+        if (usuario == null) {
+            val erro = ServerError(
+                HttpStatusCode.NotFound.value,
+                "Você não está logado. Efetue login para utilizar as funcionalidades da API.")
+            call.respond(HttpStatusCode.NotFound, erro)
+        } else {
+            val localCombustivel: Revenda = call.receive<Revenda>()
+            val localCadastrado = bancoprecos.cadastraLocalCombustivel(
+                localCombustivel.regiao,
+                localCombustivel.siglaEstado,
+                localCombustivel.municipio,
+                localCombustivel.nome,
+                localCombustivel.cnpj,
+                localCombustivel.bandeira
+            )
+            novoLoc.regiao = localCadastrado.regiao
+            novoLoc.siglaEstado = localCadastrado.siglaEstado
+            novoLoc.municipio = localCadastrado.municipio
+            novoLoc.nome = localCadastrado.nome
+            novoLoc.cnpj = localCadastrado.cnpj
+            novoLoc.bandeira = localCadastrado.bandeira
+            call.respond(localCadastrado)
+        }
     }
     return novoLoc
 }
 
 fun Route.cadastraPrecoCombustivel(localCad: Revenda) {
     post("/commoditie/combustivel"){
-        val precoCombustivel: Combustivel = call.receive<Combustivel>()
-        precoCombustivel.local.regiao = localCad.regiao
-        precoCombustivel.local.siglaEstado = localCad.siglaEstado
-        precoCombustivel.local.municipio = localCad.municipio
-        precoCombustivel.local.nome = localCad.nome
-        precoCombustivel.local.cnpj = localCad.cnpj
-        precoCombustivel.local.bandeira = localCad.bandeira
-        val precoCadastrado = bancoprecos.cadastraPrecoCombustivel(
-            precoCombustivel.tipo,
-            precoCombustivel.data,
-            precoCombustivel.valor,
-            precoCombustivel.local
-        )
-        call.respond(precoCadastrado)
+        val usuario = bancoprecos.usuarioAtivo
+        if (usuario == null) {
+            val erro = ServerError(
+                HttpStatusCode.NotFound.value,
+                "Você não está logado. Efetue login para utilizar as funcionalidades da API.")
+            call.respond(HttpStatusCode.NotFound, erro)
+        } else {
+            val precoCombustivel: Combustivel = call.receive<Combustivel>()
+            precoCombustivel.local.regiao = localCad.regiao
+            precoCombustivel.local.siglaEstado = localCad.siglaEstado
+            precoCombustivel.local.municipio = localCad.municipio
+            precoCombustivel.local.nome = localCad.nome
+            precoCombustivel.local.cnpj = localCad.cnpj
+            precoCombustivel.local.bandeira = localCad.bandeira
+            val precoCadastrado = bancoprecos.cadastraPrecoCombustivel(
+                precoCombustivel.tipo,
+                precoCombustivel.data,
+                precoCombustivel.valor,
+                precoCombustivel.local
+            )
+            call.respond(precoCadastrado)
+        }
     }
 }
 
 fun Route.consultaMediaEstado() {
     get("/precos/media") {
-        val consulta = call.receive<Consulta>()
-        val consultaRealizada = bancoprecos.consultaMediaEstado(
-            consulta.tipoCombustivel!!,
-            consulta.data!!,
-            consulta.UF!!)
-        call.respond(consultaRealizada)
+        val usuario = bancoprecos.usuarioAtivo
+        if (usuario == null) {
+            val erro = ServerError(
+                HttpStatusCode.NotFound.value,
+                "Você não está logado. Efetue login para utilizar as funcionalidades da API.")
+            call.respond(HttpStatusCode.NotFound, erro)
+        } else {
+            val consulta = call.receive<Consulta>()
+            val consultaRealizada = bancoprecos.consultaMediaEstado(
+                consulta.tipoCombustivel!!,
+                consulta.data!!,
+                consulta.UF!!)
+
+            call.respond(consultaRealizada)
+        }
     }
 }
 
 fun Route.consultaMenorPreco() {
     get("/precos/menor") {
-        val consulta = call.receive<ConsultaMinimo>()
-        val menorPreco = bancoprecos.consultaMenorPreco(
-            consulta.tipoCombustivel!!, consulta.data!!, consulta.municipio!!, consulta.UF!!)
-        call.respond(menorPreco)
+        val usuario = bancoprecos.usuarioAtivo
+        if (usuario == null) {
+            val erro = ServerError(
+                HttpStatusCode.NotFound.value,
+                "Você não está logado. Efetue login para utilizar as funcionalidades da API.")
+            call.respond(HttpStatusCode.NotFound, erro)
+        } else {
+            val consulta = call.receive<ConsultaMinimo>()
+            val menorPreco = bancoprecos.consultaMenorPreco(
+                consulta.tipoCombustivel!!, consulta.data!!, consulta.municipio!!, consulta.UF!!)
+            call.respond(menorPreco)
+        }
     }
 }
 
 fun Route.treinaModelo() {
     get("/precos/modelo"){
-        val escopoDesejado = call.receive<Escopo>()
-        val registros = bancoprecos.getDadosTreinamento(escopoDesejado.siglaEstado, escopoDesejado.tipoCombustivel)
-        val modeloTreinado = bancoprecos.treinaModelo(escopoDesejado.siglaEstado, escopoDesejado.tipoCombustivel, registros)
-        call.respond(modeloTreinado)
+        val usuario = bancoprecos.usuarioAtivo
+        if (usuario == null) {
+            val erro = ServerError(
+                HttpStatusCode.NotFound.value,
+                "Você não está logado. Efetue login para utilizar as funcionalidades da API.")
+            call.respond(HttpStatusCode.NotFound, erro)
+        } else {
+            val escopoDesejado = call.receive<Escopo>()
+            val registros = bancoprecos.getDadosTreinamento(escopoDesejado.siglaEstado, escopoDesejado.tipoCombustivel)
+            val modeloTreinado = bancoprecos.treinaModelo(escopoDesejado.siglaEstado, escopoDesejado.tipoCombustivel, registros)
+            call.respond(modeloTreinado)
+        }
     }
 }
 
 fun Route.calculaPrevisao() {
     get("/precos/modelo/previsao"){
-        val previsaoDesejada = call.receive<Previsao>()
-        val precoPrevisto = bancoprecos.calculaPrevisao(
-            previsaoDesejada.siglaEstado, previsaoDesejada.tipoCombustivel,
-            previsaoDesejada.valorPetroleo, previsaoDesejada.valorDolar)
-        call.respond(precoPrevisto)
+        val usuario = bancoprecos.usuarioAtivo
+        if (usuario == null) {
+            val erro = ServerError(
+                HttpStatusCode.NotFound.value,
+                "Você não está logado. Efetue login para utilizar as funcionalidades da API.")
+            call.respond(HttpStatusCode.NotFound, erro)
+        } else {
+            val previsaoDesejada = call.receive<Previsao>()
+            val precoPrevisto = bancoprecos.calculaPrevisao(
+                previsaoDesejada.siglaEstado, previsaoDesejada.tipoCombustivel,
+                previsaoDesejada.valorPetroleo, previsaoDesejada.valorDolar)
+            call.respond(precoPrevisto)
+        }
     }
 }
