c7b5500b5ebcf463710226a8b1d06ccd940259fa
brunojviana <brunojviana@hotmail.com>
2021-06-15 23:04:05 -0300

    make it better

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.execution.ProgramText.Companion.from
+import org.jetbrains.kotlin.contracts.model.structure.UNKNOWN_COMPUTATION.type
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val ktorVersion: String by project
